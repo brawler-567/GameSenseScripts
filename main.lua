5bbe55702c443eb1f345273ac3f63f4e45f27777
brawler-567 <67509313+brawler-567@users.noreply.github.com>
2024-12-05 23:23:22 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,4 @@
 workspace.DescendantAdded:Connect(function(descendant)
-    if descendant:IsA("ParticleEmitter") then
-        descendant.Rate = descendant.Rate * 10
-    end
-end)
-
-workspace.DescendantAdded:Connect(function(descendant)
     if descendant.Name == "base" and descendant:IsA("BasePart") then
         local player = game.Players.LocalPlayer
         if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
@@ -22,32 +16,57 @@ end)
 workspace.DescendantAdded:Connect(function(descendant)
     if descendant.Name == "eye" or descendant.Name == "elkman" or descendant.Name == "Rush" or descendant.Name == "Worm" or descendant.Name == "eyePrime" then
         descendant:Destroy()
+        game.ReplicatedStorage.SendRush.Carnation.tinnitus.Playing = false
+        game.StarterGui:SetCore("SendNotification", {
+                Title = "entity detected",
+                Text = "entity has been removed",
+                Duration = 2.3
+            })
+        end
     end
 end)
 
 local players = game:GetService("Players")
 local localPlayer = players.LocalPlayer
 
-while true do
+local function deleyeGui()
     local eyeGui = localPlayer:FindFirstChild("PlayerGui"):FindFirstChild("eyegui")
-    if eyeGui then
-        eyeGui:Destroy()
-    end
-    task.wait(0.1)
+    eyeGui:Destroy()
+end
+if eyeGui then
+  deleyeGui()
+  game.StarterGui:SetCore("SendNotification", {
+                Title = "eyeGui detected",
+                Text = "eyeGui has been removed",
+                Duration = 2.3
+            })
+   end
 end
 
-while true do
-    local smileGui = localPlayer:FindFirstChild("PlayerGui"):FindFirstChild("smilegui")
-    if smileGui then
-        smileGui:Destroy()
-    end
-    task.wait(0.1)
+local function delsmileGui()
+    local smileGui = localPlayer:FindFirstChild("PlayerGui"):FindFirstChild("smileGui")
+    smileGui:Destroy()
+end
+if smilegui then
+  delsmileGui()
+  game.StarterGui:SetCore("SendNotification", {
+                Title = "smilefaceGui detected",
+                Text = "smilefaceGui has been removed",
+                Duration = 2.3
+            })
+   end
 end
 
-while true do
-    local FUNNYGOATMAN = localPlayer:FindFirstChild("PlayerGui"):FindFirstChild("GOATPORT")
-    if FUNNYGOATMAN then
-        FUNNYGOATMAN:Destroy()
-    end
-    task.wait(0.1)
+local function delfunnygoatman()
+    local funnygoatman = localPlayer:FindFirstChild("PlayerGui"):FindFirstChild("GOATPORT")
+    funnygoatman:Destroy()
+end
+if funnygoatman then
+  delfunnygoatman()
+  game.StarterGui:SetCore("SendNotification", {
+                Title = "funnygoatmanGui detected",
+                Text = "funnygoatmanGui has been removed",
+                Duration = 2.3
+            })
+   end
 end
