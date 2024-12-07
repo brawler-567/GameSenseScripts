2a0315b9b6fbb140820c70ce8adb0ab6cc36f4cf
brawler-567 <67509313+brawler-567@users.noreply.github.com>
2024-12-07 18:25:26 +0300

    Update Esp.lua

--- Esp.lua
+++ Esp.lua
@@ -1,17 +1,18 @@
-workspace.Rooms.DescendantAdded:Connect(function(descendant)
-    if descendant:IsA("Model") and descendant.Name == Door then
-        hg = Instance.new("Highlight", descendant)
+while true do
+    for n = 1, math.huge do
+        local v = workspace.Rooms.(n).Door.Door
+        hg = Instance.new("Highlight", v)
         hg.FillColor = Color3.fromRGB(241, 196, 15)
         hg.FillTransparency = 0.5
-    
-        bb = Instance.new('BillboardGui', descendant)
-        bb.Adornee = descendant
+
+        bb = Instance.new('BillboardGui', v)
+        bb.Adornee = v
         bb.ExtentsOffset = Vector3.new(0, 1, 0)
         bb.AlwaysOnTop = true
         bb.Size = UDim2.new(0, 6, 0, 6)
         bb.StudsOffset = Vector3.new(0, 1, 0)
         bb.Name = "BillBoard"
-    
+
         txtlbl = Instance.new('TextLabel', bb)
         txtlbl.ZIndex = 10
         txtlbl.BackgroundTransparency = 1
@@ -22,7 +23,7 @@ workspace.Rooms.DescendantAdded:Connect(function(descendant)
         txtlbl.Text = "Door " .. v.Name
         txtlbl.TextStrokeTransparency = 0.5
         txtlbl.TextColor3 = Color3.fromRGB(241, 196, 15)
-    
+
         txtlbl2 = Instance.new('TextLabel', bb)
         txtlbl2.ZIndex = 10
         txtlbl2.BackgroundTransparency = 1
@@ -34,5 +35,10 @@ workspace.Rooms.DescendantAdded:Connect(function(descendant)
         txtlbl2.Name = "Dist"
         txtlbl2.TextStrokeTransparency = 0.5
         txtlbl2.TextColor3 = Color3.fromRGB(241, 196, 15)
+                    
+        if workspace.Rooms.(n).Door.Door then
+            workspace.Rooms.(n).Door.Door.BillBoard.Dist.Text = round((game.Players.LocalPlayer.Character.PrimaryPart.Position - workspace.Rooms.(n).Door.Door.Position).magnitude, 1) .. " Studs"
+        end
+        task.wait(1)
     end
-end)
+end
