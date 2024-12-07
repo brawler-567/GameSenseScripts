4160ab1d315cf16d1be983f3648604fe3803f769
brawler-567 <67509313+brawler-567@users.noreply.github.com>
2024-12-07 18:35:17 +0300

    Update Esp.lua

--- Esp.lua
+++ Esp.lua
@@ -1,19 +1,21 @@
 while true do
     for n = 1, math.huge do
         local v = workspace.Rooms.(n).Door.Door
-        hg = Instance.new("Highlight", v)
+        hg = Instance.new("Highlight")
         hg.FillColor = Color3.fromRGB(241, 196, 15)
         hg.FillTransparency = 0.5
+        hg.Parent = v
 
-        bb = Instance.new('BillboardGui', v)
+        bb = Instance.new('BillboardGui')
         bb.Adornee = v
         bb.ExtentsOffset = Vector3.new(0, 1, 0)
         bb.AlwaysOnTop = true
         bb.Size = UDim2.new(0, 6, 0, 6)
         bb.StudsOffset = Vector3.new(0, 1, 0)
         bb.Name = "BillBoard"
+        bb.Parent = v
 
-        txtlbl = Instance.new('TextLabel', bb)
+        txtlbl = Instance.new('TextLabel')
         txtlbl.ZIndex = 10
         txtlbl.BackgroundTransparency = 1
         txtlbl.Position = UDim2.new(0, 0, 0, -45)
@@ -23,8 +25,9 @@ while true do
         txtlbl.Text = "Door " .. v.Name
         txtlbl.TextStrokeTransparency = 0.5
         txtlbl.TextColor3 = Color3.fromRGB(241, 196, 15)
+        txtlbl.Parent = bb
 
-        txtlbl2 = Instance.new('TextLabel', bb)
+        txtlbl2 = Instance.new('TextLabel')
         txtlbl2.ZIndex = 10
         txtlbl2.BackgroundTransparency = 1
         txtlbl2.Position = UDim2.new(0, 0, 0, -15)
@@ -35,9 +38,10 @@ while true do
         txtlbl2.Name = "Dist"
         txtlbl2.TextStrokeTransparency = 0.5
         txtlbl2.TextColor3 = Color3.fromRGB(241, 196, 15)
+        txtlbl2.Parent = bb
                     
-        if workspace.Rooms.(n).Door.Door then
-            workspace.Rooms.(n).Door.Door.BillBoard.Dist.Text = round((game.Players.LocalPlayer.Character.PrimaryPart.Position - workspace.Rooms.(n).Door.Door.Position).magnitude, 1) .. " Studs"
+        if v then
+            v.BillBoard.Dist.Text = round((game.Players.LocalPlayer.Character.PrimaryPart.Position - v.Position).magnitude, 1) .. " Studs"
         end
         task.wait(1)
     end
