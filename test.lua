80699b4b7efb2955f733643a889e6ad391901f2b
brawler-567 <67509313+brawler-567@users.noreply.github.com>
2024-12-08 12:19:47 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,30 +1,24 @@
 local guiGS = Instance.new("ImageButton")
-guiGS.Image = "rbxassetid://8126130334"
 local players = game:GetService("Players")
 local plr = players.LocalPlayer
+guiGS.Image = "rbxassetid://8126130334"
 guiGS.Parent = plr:WaitForChild("PlayerGui")
 
 local function toggleGui()
     guiGS.Visible = not guiGS.Visible
 end
-
 guiGS.Activated:Connect(toggleGui)
 
 
 local TweenService = game:GetService("TweenService")
 local size = UDim2.new(guiGS.Size.X.Scale, guiGS.Size.X.Offset, guiGS.Size.Y.Scale, guiGS.Size.Y.Offset)
-
-
 local tweenInfo = TweenInfo.new(
-    0.5, -- Время tween'а в секундах
-    Enum.EasingStyle.Quad, -- Стиль tween'а
-    Enum.EasingDirection.Out -- Направление tween'а
+    0.5,
+    Enum.EasingStyle.Quad,
+    Enum.EasingDirection.Out
 )
-
--- Создаем tween' для передвижения imagebutton'а
 local tween = TweenService:Create(guiGS, tweenInfo, {Size = size})
 
--- При нажатии imagebutton'а выполняем tween' и отменяем его при отпускании
 guiGS.Activated:Connect(function()
     tween:Play()
 end)
