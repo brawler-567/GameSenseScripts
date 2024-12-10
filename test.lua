bade349dd297ff8e04a631803e75de4154172e96
brawler-567 <67509313+brawler-567@users.noreply.github.com>
2024-12-10 04:02:26 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,36 +1,13 @@
-local guiGS = Instance.new("ImageButton")
-local players = game:GetService("Players")
-local plr = players.LocalPlayer
-guiGS.Image = "rbxassetid://8126130334"
-guiGS.Parent = plr:WaitForChild("PlayerGui")
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local function toggleGui()
-    guiGS.Visible = not guiGS.Visible
-end
-guiGS.Activated:Connect(toggleGui)
+local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
 
-
-local TweenService = game:GetService("TweenService")
-local size = UDim2.new(guiGS.Size.X.Scale, guiGS.Size.X.Offset, guiGS.Size.Y.Scale, guiGS.Size.Y.Offset)
-local tweenInfo = TweenInfo.new(
-    0.5,
-    Enum.EasingStyle.Quad,
-    Enum.EasingDirection.Out
-)
-local tween = TweenService:Create(guiGS, tweenInfo, {Size = size})
-
-guiGS.Activated:Connect(function()
-    tween:Play()
-end)
-
-guiGS.InputBegan:Connect(function(input)
-    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-        tween:Pause()
-    end
-end)
-
-guiGS.InputEnded:Connect(function(input)
-    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-        tween:Resume()
-    end
-end)
+--[[
+Name = <string> - The name of the tab.
+Icon = <string> - The icon of the tab.
+PremiumOnly = <bool> - Makes the tab accessible to Sirus Premium users only.
+]]
