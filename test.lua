2815129487e90fd2374cb40b92709c3ef70bd0ae
brawler-567 <67509313+brawler-567@users.noreply.github.com>
2024-12-10 14:12:58 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -29,7 +29,7 @@ local Window = Rayfield:CreateWindow({
       FileName = "Key",
       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Fre1eWeek"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Key = {"Fr3eWeek"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
 --Tabs
@@ -106,7 +106,25 @@ local RemoveEntitiesMain = MainTab:CreateButton({
 
 --Legit
 
-
+local LeaverEspSec = LegitTab:CreateSection("LeverEsp")
+local Toggle = Tab:CreateToggle({
+   Name = "eye",
+   CurrentValue = false,
+   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(eye)
+   if eye = true then
+      workspace.DescendantAdded:Connect(function(descendant)
+         if descendant.Name == "eye" then
+            game.StarterGui:SetCore("SendNotification", {
+            Title = "eye",
+            Text = "eye has been spawned",
+            Duration = 3
+         })
+         end
+      end)
+   end
+   end,
+})
 
 
 --Addons
