8c6b48aaea3f74d2afc1cb413d956ecfaacb5aee
brawler-567 <67509313+brawler-567@users.noreply.github.com>
2024-12-10 14:44:00 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -103,24 +103,7 @@ local RemoveEntitiesMain = MainTab:CreateButton({
 
 local LeaverEspSec = LegitTab:CreateSection("LeverEsp")
 local Toggle = LegitTab:CreateToggle({
-   Name = "eye",
-   CurrentValue = false,
-   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(eye)
-   if eye = true then
-      workspace.DescendantAdded:Connect(function(descendant)
-         if descendant.Name == "eye" then
-            Rayfield:Notify({
-               Title = "eye",
-               Content = "eye has been spawned!",
-               Duration = 3,
-               Image = "rewind",
-            })
-         end
-      end)
-   end
-   end,
-})
+
 
 
 --Addons
