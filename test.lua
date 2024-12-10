6e173ebcf2a9218839807860160f1984c14206f6
brawler-567 <67509313+brawler-567@users.noreply.github.com>
2024-12-10 05:10:44 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -48,3 +48,13 @@ local BoostFPS = Main:CreateButton({
 })
 
 local RemoveEntities = Main:CreateSection("Remove Entities")
+
+local Toggle = Tab:CreateToggle({
+   Name = "Remove Canva",
+   CurrentValue = false,
+   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+   -- The function that takes place when the toggle is pressed
+   -- The variable (Value) is a boolean on whether the toggle is true or false
+   end,
+})
