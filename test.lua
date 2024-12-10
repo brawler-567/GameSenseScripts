03034b4a216fc08430f50bee2cc7902bd8cc2a34
brawler-567 <67509313+brawler-567@users.noreply.github.com>
2024-12-10 20:47:59 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -107,7 +107,50 @@ local Toggle = Tab:CreateToggle({
    CurrentValue = false,
    Flag = "LeverEsp", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
-   
+   workspace.DescendantAdded:Connect(function(descendant)
+       if descendant.Name == "base" and descendant:IsA("BasePart") then
+           local player = game.Players.LocalPlayer
+           if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+              if Value = true then
+                 v = descendant
+                 hg = Instance.new("Highlight", v)
+                 hg.FillColor = Color3.fromRGB(241, 196, 15)
+                 hg.FillTransparency = 0.5
+                 
+                 bb = Instance.new('BillboardGui', v)
+                 bb.Adornee = v
+                 bb.ExtentsOffset = Vector3.new(0, 1, 0)
+                 bb.AlwaysOnTop = true
+                 bb.Size = UDim2.new(0, 6, 0, 6)
+                 bb.StudsOffset = Vector3.new(0, 1, 0)
+                 bb.Name = "BillBoard"
+    
+                 txtlbl = Instance.new('TextLabel', bb)
+                 txtlbl.ZIndex = 10
+                 txtlbl.BackgroundTransparency = 1
+                 txtlbl.Position = UDim2.new(0, 0, 0, -45)
+                 txtlbl.Size = UDim2.new(1, 0, 10, 0)
+                 txtlbl.Font = 'ArialBold'
+                 txtlbl.FontSize = 'Size12'
+                 txtlbl.Text = "Lever " .. v.Name
+                 txtlbl.TextStrokeTransparency = 0.5
+                 txtlbl.TextColor3 = Color3.fromRGB(241, 196, 15)
+    
+                 txtlbl2 = Instance.new('TextLabel', bb)
+                 txtlbl2.ZIndex = 10
+                 txtlbl2.BackgroundTransparency = 1
+                 txtlbl2.Position = UDim2.new(0, 0, 0, -15)
+                 txtlbl2.Size = UDim2.new(1, 0, 10, 0)
+                 txtlbl2.Font = 'ArialBold'
+                 txtlbl2.FontSize = 'Size12'
+                 txtlbl2.Text = "? Studs"
+                 txtlbl2.Name = "Dist"
+                 txtlbl2.TextStrokeTransparency = 0.5
+                 txtlbl2.TextColor3 = Color3.fromRGB(241, 196, 15)
+              end
+           end
+       end
+   end)
    end,
 })
 
