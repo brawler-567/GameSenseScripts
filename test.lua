17128704da9482c5e3dc44e4ffb6ed7e037c1473
brawler-567 <67509313+brawler-567@users.noreply.github.com>
2024-12-10 05:21:41 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -52,6 +52,37 @@ local RemoveEntities = Main:CreateSection("Remove Entities")
 local Button = Main:CreateButton({
    Name = "Remove all entities",
    Callback = function()
-   
+   workspace.DescendantAdded:Connect(function(descendant)
+      if descendant.Name == "eye" or descendant.Name == "elkman" or descendant.Name == "Rush" or descendant.Name == "Worm" or descendant.Name == "eyePrime" then
+         descendant:Destroy()
+      end
+   end)
+   local RS = game:GetService("ReplicatedStorage")
+   RS.eyegui:Destroy()
+   RS.smilegui:Destroy()
+   RS.SendRush:Destroy()
+   RS.SendWorm:Destroy()
+   RS.SendSorrow:Destroy()
+   RS.SendGoatman:Destroy()
+   task.wait(0.1)
+   RS.Worm:Destroy()
+   RS.elkman:Destroy()
+   task.wait(0.1)
+   RS.QuickNotes.Eye:Destroy()
+   RS.QuickNotes.Rush:Destroy()
+   RS.QuickNotes.Sorrow:Destroy()
+   RS.QuickNotes.elkman:Destroy()
+   RS.QuickNotes.EyePrime:Destroy()
+   RS.QuickNotes.SlugFish:Destroy()
+   RS.QuickNotes.FakeDoor:Destroy()
+   RS.QuickNotes.SleepyHead:Destroy()
+   while true do
+      local players = game:GetService("Players")
+      local plr = players.LocalPlayer
+      local SmileGui = plr:FindFirstChild("PlayerGui"):FindFirstChild("smilegui")
+      if SmileGui then
+         SmileGui:Destroy() 
+      end
+   task.wait(0.1)
    end,
 })
