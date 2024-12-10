7fa7d30bc4ed7c43a9c5bf244e1967967f8f031d
brawler-567 <67509313+brawler-567@users.noreply.github.com>
2024-12-10 10:09:29 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -63,7 +63,7 @@ local LeversMain = MainTab:CreateButton({
 })
 
 local RemoveEntitiesSec = MainTab:CreateSection("Remove Entities")
-local RemoveEntities = MainTab:CreateButton({
+local RemoveEntitiesMain = MainTab:CreateButton({
    Name = "Remove all entities",
    Callback = function()
    local RS = game:GetService("ReplicatedStorage")
@@ -116,21 +116,6 @@ local BoostFPS = Addons:CreateButton({
    workspace.DescendantAdded:Connect(function(descendant)
       if descendant:IsA("ParticleEmitter") then
          descendant.Rate = descendant.Rate * 6
-      end
-   end)
-   end,
-})
-local Slider = Addons:CreateSlider({
-   Name = "BoostFPS",
-   Range = {1, 10},
-   Increment = 1,
-   Suffix = "removes special effects",
-   CurrentValue = 2,
-   Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(R)
-   workspace.DescendantAdded:Connect(function(descendant)
-      if descendant:IsA("ParticleEmitter") then
-         descendant.Rate = descendant.Rate * R
       end
    end)
    end,
