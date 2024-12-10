ff2efb5490e9fd2694c2db8cac99c9105bc469ca
brawler-567 <67509313+brawler-567@users.noreply.github.com>
2024-12-10 14:02:10 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -106,25 +106,7 @@ local RemoveEntitiesMain = MainTab:CreateButton({
 
 --Legit
 
-local LeaverEspSec = LegitTab:CreateSection("LeverEsp")
-local Toggle = Tab:CreateToggle({
-   Name = "eye",
-   CurrentValue = false,
-   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(eye)
-   if eye = true then
-      workspace.DescendantAdded:Connect(function(descendant)
-         if descendant.Name == "eye" then
-            game.StarterGui:SetCore("SendNotification", {
-            Title = "eye",
-            Text = "eye has been spawned",
-            Duration = 3
-         })
-         end
-      end)
-   end
-   end,
-})
+
 
 
 --Addons
