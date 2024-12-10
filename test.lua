845c651600eda8a567d64e54f7229ac8b3f3fed4
brawler-567 <67509313+brawler-567@users.noreply.github.com>
2024-12-10 04:59:22 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -36,7 +36,6 @@ local Main = Window:CreateTab("Main", "rewind")
 local Legit = Window:CreateTab("legit", "rewind")
 
 local Section = Main:CreateSection("Addons")
-local BoostFPSMain = Section:CreateLabel("BoostFPS", "rewind")
 local BoostFPS = Section:CreateButton({
    Name = "BoostFPS",
    Callback = function()
