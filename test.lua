b223d28083a405d010a753f6cd36480c01aa3598
brawler-567 <67509313+brawler-567@users.noreply.github.com>
2024-12-11 11:12:54 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -107,8 +107,19 @@ local Toggle = Tab:CreateToggle({
    CurrentValue = false,
    Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
-   -- The function that takes place when the toggle is pressed
-   -- The variable (Value) is a boolean on whether the toggle is true or false
+   workspace.DescendantAdded:Connect(function(descendant)
+       if descendant.Name == "base" and descendant:IsA("BasePart") then
+           local player = game.Players.LocalPlayer
+           if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+               Rayfield:Notify({
+                  Title = "Levers Activated",
+                  Content = "Door has been opened!",
+                 Duration = 1.5,
+                 Image = "rewind",
+               })
+           end
+       end
+   end)
    end,
 })
 
